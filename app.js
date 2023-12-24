da42dc65f5698956c7ec74cea43b0dd2d5b06a7c
mrGolbya <mr.golbya@gmail.com>
2023-12-25 01:18:11 +0300

    feat: add buttons in js

--- app.js
+++ app.js
@@ -1,6 +1,69 @@
+(async function () {
 let response = await fetch('./buttons.json')
 let buttons = await response.json()
-console.log(buttons)
+
+document.body.insertAdjacentHTML('beforeend', 
+`<div class="social__buttons">
+<div class="container">
+  <div class="social__wrapper">
+    
+    <div class="button"
+      ><img
+        src="images/message.svg"
+        class="message"
+        alt="message"
+        width="36"
+        height="36" /><span></span
+    ></div>
+  </div>
+</div>
+</div>`);
+
+(function () {
+  let step = [] 
+
+  for(let i=1; i<=buttons.length; i++){
+    step.push(i*2+1)
+  }
+  
+console.log(step)
+
+  for (let i of buttons){
+    let socialWrapper = document.querySelector('.social__wrapper')
+    socialWrapper.insertAdjacentHTML('afterbegin', `<a href=${i.href}
+    target="_blank"
+    rel="nofollow noopener noreferrer"
+    class=${i.name}
+  >
+    <img src=${i.img} alt=${i.name} width="50" height="50"
+  /></a>`) 
+  
+  
+  // console.log(document.querySelector('.social__wrapper a').animate([
+  //       // key frames
+  //       { transform: 'translateY(0px)' },
+  //       { transform: `translateY(-${+i.width+1}rem)` }
+  //     ], {
+  //       // sync options
+  //       duration: 2000,
+  //       iterations: Infinity
+  //     }))
+  }
+}());
+
+// document.querySelectorAll('.social__wrapper a ').forEach((i)=>{
+//   i.animate([
+//     // key frames
+//     { transform: 'translateY(0px)' },
+//     { transform: 'translateY(-300px)' }
+//   ], {
+//     // sync options
+//     duration: 2000,
+//     iterations: Infinity
+//   });
+  
+// })
+
 
 //animation-active
 document.querySelector('.social__wrapper').addEventListener('click', (e) => {
@@ -18,10 +81,11 @@ document.querySelector('.social__wrapper').addEventListener('click', (e) => {
     document.querySelector('.social__wrapper .vk ').classList.add('animation-active-1')
     document.querySelector('.social__wrapper .wahatsapp').classList.add('animation-active-2')
     document.querySelector('.social__wrapper .mail ').classList.add('animation-active-3')
+
   }
 
-
   else {
+    
     document.querySelector('.social__buttons .button').classList.remove('active')
     document.querySelector('.social__buttons .button span').classList.remove('active')
     document.querySelector('.social__wrapper .vk ').classList.remove('animation-active-1')
@@ -33,5 +97,7 @@ document.querySelector('.social__wrapper').addEventListener('click', (e) => {
     document.querySelector('.social__wrapper .vk ').classList.add('animation-active-1-none')
     document.querySelector('.social__wrapper .wahatsapp').classList.add('animation-active-2-none')
     document.querySelector('.social__wrapper .mail ').classList.add('animation-active-3-none')
+    
   }
 })
+}());
